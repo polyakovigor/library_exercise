f3bfbbbad57744f2cf4fda9eeb28c98916422b5a
Igor <igor.polyakov@faceit-team.com>
2017-08-19 11:55:22 +0300

    app settings

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,11 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
 
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,6 +15,11 @@ module LibraryExercise
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.mongoid.logger = Logger.new($stdout, :warn)
+    config.mongoid.preload_models = false
+    config.generators do |g|
+      g.orm :mongoid
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
