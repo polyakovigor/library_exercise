40f6110c513bb12e34a61fae41e82cfdbfa729c5
Igor <igor.polyakov@faceit-team.com>
2017-08-21 18:00:40 +0300

    signup & signin pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,16 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, :last_name, presence: true, length: { maximum: 15 }
 
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  field :first_name,         type: String
+  field :last_name,          type: String
   ## Database authenticatable
-  field :email,              type: String, default: ""
-  field :encrypted_password, type: String, default: ""
+  field :email,              type: String, default: ''
+  field :encrypted_password, type: String, default: ''
 
   ## Recoverable
   field :reset_password_token,   type: String
@@ -33,6 +37,5 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
-
 
 end
